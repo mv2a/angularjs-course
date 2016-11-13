30a6ea36043efeac6d3ef9192092e7c2b36f34dc
Tiago Miranda <mv2axx@gmail.com>
2016-11-13 23:30:29 +0000

    adding files

---
+++ module1-solution/app.js
@@ -0,0 +1,15 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', DIController);
+
+LunchCheckController.$inject = ['$scope', '$filter'];
+
+function LunchCheckController ($scope, $filter) {
+  
+}
+
+
+})();
