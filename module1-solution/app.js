a892e4a415b62a615f126dcf756ae944e29a2efc
Tiago Miranda <mv2axx@gmail.com>
2016-11-14 00:07:24 +0000

    basic implementation completed

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,13 +3,23 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', DIController);
+.controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
+LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController ($scope, $filter) {
-  
+function LunchCheckController ($scope) {
+    $scope.dishes = "";
+    $scope.checkDishes = function () {
+      if ($scope.dishes) { //checks for non empty
+        if ($scope.dishes.split(",").length <= 3)
+          $scope.message = "Enjoy!"
+        else {
+          $scope.message = "Too much!"
+        }
+      } else {
+        $scope.message = "Please enter data first"
+      }
+    };
 }
-
 
 })();
